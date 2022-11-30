eaac1aac77e78a84483bbc54b4de6f5f25772ff2
Proficientdc <proficient153@gmail.com>
2022-11-30 18:40:05 +0530

    first commit

---
+++ src/components/article/Article.jsx
@@ -0,0 +1,21 @@
+"strict mode"
+import React from 'react'
+import "./article.css";
+const Article = ({imgUrl, date, title}) => {
+  return (
+    <div className='gpt3__blog-container_article'>
+      <div className='gpt3__blog-container_article-image'>
+        <img src={imgUrl} alt='blog' />
+      </div>
+      <div className='gpt3__blog-container_article-content'>
+        <div>
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>
+        <p>Read Full Artcile</p>
+      </div>
+    </div>
+  )
+}
+
+export default Article
